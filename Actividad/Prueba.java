e7bcc875020a4dbe62f7e5a00901ab477ba8b4d9
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-15 15:54:27 -0500

    Update Prueba.java

--- Actividad/Prueba.java
+++ Actividad/Prueba.java
@@ -1,7 +1,19 @@
 package Actividad;
 
+import bstreelinklistinterfgeneric.LinkedBST;
+import Exceptions.ItemDuplicated;
+
 public class Prueba {
     public static void main(String[] args) {
+LinkedBST<Integer> bst = new LinkedBST<>();
+        int[] datos = {400, 100, 700, 50, 200, 75};
 
+        try {
+            for (int valor : datos) {
+                bst.insert(valor);
+            }
+        } catch (ItemDuplicated e) {
+            System.out.println("Error al insertar: " + e.getMessage());
+        }
     }
 }
