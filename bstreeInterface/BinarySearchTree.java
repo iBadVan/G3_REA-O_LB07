99e6d726e1350237277424f3e72f0800c4a86d3d
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-15 15:15:56 -0500

    Create BinarySearchTree.java

---
+++ bstreeInterface/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package bstreeInterface;
+
+import Exceptions.ItemDuplicated;
+import Exceptions.ItemNoFound;
+import Exceptions.ExceptionIsEmpty;
+
+public interface BinarySearchTree<E> {
+    
+    void insert(E data) throws ItemDuplicated;
+
+    E search(E data) throws ItemNoFound;
+
+    void delete(E data) throws ExceptionIsEmpty;
+
+    boolean isEmpty();
+}
