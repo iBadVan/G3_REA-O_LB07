2ed7c2ac7538919839f7265e1e1ada0698e96df5
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-15 15:26:39 -0500

    Create LinkedBST.java

---
+++ bstreelinklistinterfgeneric/LinkedBST.java
@@ -0,0 +1,24 @@
+package bstreelinklistinterfgeneric;
+
+import bstreeInterface.BinarySearchTree;
+import Exceptions.ItemDuplicated;
+import Exceptions.ItemNoFound;
+import Exceptions.ExceptionIsEmpty;
+
+public class LinkedBST<E extends Comparable<E>> implements BinarySearchTree<E> {
+
+    class Node {
+        public E data;
+        public Node left, right;
+
+        public Node(E data) {
+            this(data, null, null);
+        }
+
+        public Node(E data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
